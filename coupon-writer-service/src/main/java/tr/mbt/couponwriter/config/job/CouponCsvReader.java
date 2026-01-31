d4207f04608bc433df72c0a7e6440caa58d3cde5
Berker Turan <turanberker@yahoo.com>
2026-01-31 16:25:08 +0300

    CouponCsvReader header check from constants

--- coupon-writer-service/src/main/java/tr/mbt/couponwriter/config/job/CouponCsvReader.java
+++ coupon-writer-service/src/main/java/tr/mbt/couponwriter/config/job/CouponCsvReader.java
@@ -4,8 +4,11 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.core.io.Resource;
+import tr.mbt.coupon.coupondata.constants.UploadedFileConstants;
 
 import java.time.LocalDate;
+
+import static tr.mbt.coupon.coupondata.constants.UploadedFileConstants.*;
 
 public class CouponCsvReader {
 
@@ -16,25 +19,19 @@ public class CouponCsvReader {
 
         DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
         tokenizer.setDelimiter(",");
-        tokenizer.setNames(
-                "code",
-                "type",
-                "discountType",
-                "discountAmount",
-                "expiryDate",
-                "maxUsages"
-        );
+        tokenizer.setNames(EXPECTED_HEADERS);
+        tokenizer.setStrict(true);
 
         DefaultLineMapper<CouponCsvRow> lineMapper = new DefaultLineMapper<>();
         lineMapper.setLineTokenizer(tokenizer);
         lineMapper.setFieldSetMapper(fieldSet -> {
             CouponCsvRow row = new CouponCsvRow();
-            row.setCode(fieldSet.readString("code"));
-            row.setType(fieldSet.readString("type"));
-            row.setDiscountType(fieldSet.readString("discountType"));
-            row.setDiscountAmount(fieldSet.readBigDecimal("discountAmount"));
-            row.setExpiryDate(LocalDate.parse(fieldSet.readString("expiryDate")));
-            row.setMaxUsages(fieldSet.readLong("maxUsages"));
+            row.setCode(fieldSet.readString(CODE));
+            row.setType(fieldSet.readString(TYPE));
+            row.setDiscountType(fieldSet.readString(DISCOUNT_TYPE));
+            row.setDiscountAmount(fieldSet.readBigDecimal(DISCOUNT_AMOUNT));
+            row.setExpiryDate(LocalDate.parse(fieldSet.readString(EXPIRY_DATE)));
+            row.setMaxUsages(fieldSet.readLong(MAX_USAGE));
             return row;
         });
 
